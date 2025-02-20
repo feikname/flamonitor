69e0ca46587856ee0474c0d9c924f3fd739244d3
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-02-20 16:05:44 -0300

    fix bug python not available in PATH right after install

--- client/3-install-pip-requirements.ps1
+++ client/3-install-pip-requirements.ps1
@@ -1,2 +1,3 @@
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 python.exe -m pip install --upgrade pip
 python.exe -m pip install -r flamonitor-v0.1.0-dist/requirements.txt
--- client/do-all.ps1
+++ client/do-all.ps1
@@ -1,5 +1,6 @@
 & .\1-install-choco.ps1
 & .\2-install-python-3.11-from-choco.ps1
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 & .\3-install-pip-requirements.ps1
 & .\4-move-to-program-files.ps1
 & .\5-register-flamonitor-task.ps1

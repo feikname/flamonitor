57dad9dbb06ea67af40423f250ebc0d2542cc8aa
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2025-02-20 15:38:45 -0300

    esqueci de registrar para rodar a cada 5min

--- client/5-register-flamonitor-task.ps1
+++ client/5-register-flamonitor-task.ps1
@@ -3,10 +3,10 @@ $Trigger = New-ScheduledTaskTrigger -AtStartup
 $Principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
 $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Description "Runs flamonitor every 5 minutes after startup"
 
-Register-ScheduledTask -TaskName "flamonitor v0.1.0" -InputObject $Task
+Register-ScheduledTask -TaskName "flamonitor v0.1.1" -InputObject $Task
 
 #https://superuser.com/questions/1373275/how-to-schedule-a-task-with-powershell-to-run-every-hour-monday-to-friday-betwe
-# $Task = Get-ScheduledTask -TaskName "flamonitor v0.1.0"
-# $Task.Triggers.Repetition.Interval = "PT5M"
-# $Task.Triggers.Repetition.Duration = ""
-# $Task | Set-ScheduledTask -User "NT AUTHORITY\SYSTEM"
+$Task = Get-ScheduledTask -TaskName "flamonitor v0.1.1"
+$Task.Triggers.Repetition.Interval = "PT5M"
+$Task.Triggers.Repetition.Duration = ""
+$Task | Set-ScheduledTask -User "NT AUTHORITY\SYSTEM"
